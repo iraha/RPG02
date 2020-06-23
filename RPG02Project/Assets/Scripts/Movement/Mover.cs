726f9ba334fa3975963722852e2b0004c330d442
Masayoshi Iraha <urasho6.12@gmail.com>
2020-06-24 00:48:56 +0900

    add player character

---
+++ RPG02Project/Assets/Scripts/Movement/Mover.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Mover : MonoBehaviour
+{
+
+    [SerializeField] Transform target;
+
+    NavMeshAgent navMeshAgent;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetMouseButton(0)) 
+        {
+            MoveToCursor();
+        }
+        //Debug.DrawRay(lastRay.origin, lastRay.direction * 100);
+
+        UpdateAnimator();
+
+
+    }
+
+    private void MoveToCursor() 
+    {
+
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        bool hasHit = Physics.Raycast(ray, out hit);
+        
+        if (hasHit) 
+        {
+            GetComponent<NavMeshAgent>().destination = hit.point;
+        }
+        
+    }
+
+    public void UpdateAnimator()
+    {
+        Vector3 velocity = GetComponent<NavMeshAgent>().velocity;
+        Vector3 localVelocity = transform.InverseTransformDirection(velocity);
+        float speed = localVelocity.z;
+        GetComponent<Animator>().SetFloat("forwardSpeed", speed);
+    }
+    
+}
